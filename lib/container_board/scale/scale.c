b672644409060e1f17c327b53dfb95096955c8d2
Lisa tb <lisatebraak@gmail.com>
2025-11-13 17:11:51 +0100

    error handling scale

--- lib/container_board/scale/scale.c
+++ lib/container_board/scale/scale.c
@@ -1,27 +1,58 @@
 #include "scale.h"
 #include <logging.h>
+#include <result.h>
 
-static float OFFSET; //offset to tare
-static float CALIBRATION_FACTOR; //reading/known weight
+static const char TAG[] = "SCALE";
 
-void init(scale_t* scale, float known_weight) {
+static float OFFSET = 0; //offset to tare
+static float CALIBRATION_FACTOR = 0; //reading/known weight
+
+result_t init(scale_t* scale, float known_weight) {
+    scale->isOn = true;
     tare(scale);
     calibrate(scale, known_weight);
+    return RESULT_OK;
 }
 
-void tare(scale_t* scale) {
+result_t tare(scale_t* scale) {
+    if (!scale->isOn) {
+        return RESULT_ERR_NOT_INITIALIZED;
+    }
     OFFSET = scale->raw_data;
+    return RESULT_OK;
 }
 
-void calibrate(scale_t* scale, float known_weight) {
-    CALIBRATION_FACTOR = read_tared(scale) / known_weight;
+result_t calibrate(scale_t* scale, float known_weight) {
+    if (!scale->isOn || OFFSET == 0) {
+        return RESULT_ERR_NOT_INITIALIZED;
+    }
+    LOG(LOG_INFO, TAG, "Make sure you have placed a known weight on the scale");
+    float weight_tared;
+    read_tared(scale, weight_tared);
+    CALIBRATION_FACTOR = weight_tared / known_weight;
+    return RESULT_OK;
 }
 
-float read_weight(scale_t* scale) {
-    return read_tared(scale) / CALIBRATION_FACTOR;
+result_t read_weight(scale_t* scale, float weight) {
+    if (!scale->isOn || OFFSET == 0 || CALIBRATION_FACTOR == 0) {
+        return RESULT_ERR_NOT_INITIALIZED;
+    }
+    float weight_tared;
+    read_tared(scale, weight_tared);
+    weight =  weight_tared / CALIBRATION_FACTOR;
+    return RESULT_OK;
 }
 
-static float read_tared(scale_t* scale) {
-    return scale->raw_data - OFFSET;
+result_t turnOff(scale_t* scale) {
+    scale->isOn = false;
+    return RESULT_OK;
+}
+
+static result_t read_tared(scale_t* scale, float weight_tared) {
+    if (!scale->isOn) {
+        return RESULT_ERR_NOT_INITIALIZED;
+    }
+    weight_tared = scale->raw_data - OFFSET;
+    return RESULT_OK;
 }
 
--- lib/container_board/scale/scale.h
+++ lib/container_board/scale/scale.h
@@ -1,7 +1,10 @@
 #ifndef SCALE_H
 #define SCALE_H
+#include <result.h>
+#include <stdbool.h>
 
 typedef struct {
+    bool isOn;
     float raw_data;
 } scale_t;
 
@@ -10,35 +13,48 @@ typedef struct {
  * (The neccessary data being OFFSET and CALIBRATION_FACTOR)
  * 
  * @param scale scale struct
- * NOTE: there should be a weight on the scale when doing this
+ * @return RESULT_OK if OK
+ * NOTE: a weight should be placed on the scale at some point when doing this
  */
-void init(scale_t* scale, float known_weight);
+result_t init(scale_t* scale, float known_weight);
 
 /**
  * @brief taring the scale sets the OFFSET to the given value
  * 
  * @param scale scale struct
- * NOTE: there should be a weight on the scale when doing this
+ * @return RESULT_OK if OK, RESULT_ERR_NOT_INITIALIZED if the scale is not on
  */
-void tare(scale_t* scale);
+result_t tare(scale_t* scale);
 
 /**
  * @brief calibrating the scale retreives the CALIBRATION_FACTOR
  * 
  * @param scale scale struct
  * @param known_weight weight of the calibration weight
+ * @return RESULT_OK if OK, RESULT_ERR_NOT_INITIALIZED if the scale is not on or not tared
  * NOTE: there should be a weight on the scale when doing this
  */
-void calibrate(scale_t* scale, float known_weight);
+result_t calibrate(scale_t* scale, float known_weight);
 
 /**
  * @brief reads the accurate weight (desired value)
  * Subtracts the OFFSET and then divides by the CALIBRATION_FACTOR
  * 
  * @param scale scale struct
- * @return float accurate weight reading
+ * @param weight var to store accurate weight reading
+ * @return result_t RESULT_OK if OK, RESULT_ERR_NOT_INITIALIZED if the scale is not on or not tared or calibrated
  */
-float read_weight(scale_t* scale);
+result_t read_weight(scale_t* scale, float weight);
+
+/**
+ * @brief turns off the scale
+ * 
+ * @param scale scale struct
+ * @return result_t RESULT_OK if ok
+ */
+result_t turnOff(scale_t* scale) {
+    scale->isOn = false;
+}
 
 /**
  * @brief HELPER FUNCTION to get a weight read with taring included
@@ -46,8 +62,9 @@ float read_weight(scale_t* scale);
  * NOTE: the calibration factor is NOT used here
  * 
  * @param scale scale struct
- * @return float tared weight
+ * @param weight_tared var to store the output
+ * @return RESULT_OK if OK, RESULT_ERR_NOT_INITIALIZED if the scale is not on
  */
-static float read_tared(scale_t* scale);
+static result_t read_tared(scale_t* scale, float weight_tared);
 
 #endif 
