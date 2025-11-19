a0bfd32efea33ab29759510a8b7120c758e44eb3
İrem Çıvgıner <i.civginer@student.utwente.nl>
2025-11-19 16:18:07 +0100

    added fixes staged for parser and motor

--- lib/driving_board/motor/motor.c
+++ lib/driving_board/motor/motor.c
@@ -1,38 +1,41 @@
 #include "logging.h"
 #include <stdint.h>
 #include "motor.h"
+#include "result.h"
 
 
 static MotorInfoCallback_t info_callback = 0;
 
 
 
+//init these in shared data for motor init just have whts different for each motor
 result_t motor_init(motor_t *motors){//I want to put the result type from logging instead for the return
 
-    for (int i = 0; i < NUM_MOTORS; i++) {
-        motor_t *motor = &motors[i];
-        motor->motor_id = i;
-        motor-> distance_to_go = 0;
-        motor->turning_radius = 0;
-        motor->turning_angle = 0;
-    }
-    return RESULT_OK;
+   for (int i = 0; i < NUM_MOTORS; i++) {
+       motor_t *motor = &motors[i];
+       motor->motor_id = i;
+       motor-> distance_to_go = 0;
+       motor->turning_radius = 0;
+       motor->turning_angle = 0;
+   }
+   return RESULT_OK;
 }//this logic would change if there was a thread running for each motor
 
 result_t motor_update(motor_t *motors, int index,
-                  float distance_to_go,
-                  float turning_radius,
-                  float turning_angle) {//must pass in a pointer to a motor struct
+                 float distance_to_go,
+                 float turning_radius,
+                 float turning_angle) {//must pass in a pointer to a motor struct
 
-    if (index < 0 || index >= NUM_MOTORS) {
-        //LOGI("Motor", "index out of bounds");
-        return RESULT_ERR_INVALID_ARG;
-    }
-    motor_t *motor = &motors[index];
-    motor->distance_to_go= distance_to_go;
-    motor->turning_radius = turning_radius;
-    motor->turning_angle = turning_angle;
-    return RESULT_OK;
+
+   if (index < 0 || index >= NUM_MOTORS) {
+       //LOGI("Motor", "index out of bounds");
+       return RESULT_ERR_INVALID_ARG;
+   }
+   motor_t *motor = &motors[index];
+   motor->distance_to_go= distance_to_go;
+   motor->turning_radius = turning_radius;
+   motor->turning_angle = turning_angle;
+   return RESULT_OK;
 }
 
 
@@ -41,17 +44,17 @@ result_t motor_update(motor_t *motors, int index,
 
 //currently stub implementatiton
 void Motor_RegisterCallback(MotorInfoCallback_t cb) {//called in main
-    info_callback = cb;
+   info_callback = cb;
 }
 
 void Motor_UpdateDiagnostics(const DrivingBoardMotorInformation_t *data) {
-    if (info_callback) {
-        info_callback(data);
-    }
+   if (info_callback) {
+       info_callback(data);
+   }
 }
 
 void MotorInfoHandler(const DrivingBoardMotorInformation_t *data) {
-    //I would start the parsing&sending process of diagnostic data here
+   //I would start the parsing&sending process of diagnostic data here
 }
 
 
--- lib/driving_board/parser/parser.c
+++ lib/driving_board/parser/parser.c
@@ -3,35 +3,37 @@
 //will use that type in the future right now it assumes I got drivingboard msg
 #include "logging.h"
 #include "string.h"
+#include "logging.h"
 #include "driving_board.pb.h"
 #include "pb_message.h"
 #include <stdint.h>
 
+static char *TAG = "MAIN";
+
 result_t DBMMsgEncode(float distance_to_go, float turning_angle, float turning_radius, pb_encoding_t* encoding_out){//pointer passing for the result encoding_out
-  if (turning_angle > 360){
-    LOGE(TAG, "Angle value redundant");
-    return RESULT_ERR_INVALID_ARG;
-  }
-  if (turning_radius < 0){
-    LOGE(TAG, "turning radius can't be negative");
-    return RESULT_ERR_INVALID_ARG;
-  }
-  if (distance_to_go < 0){
-    LOGE(TAG, "distance to go can't be negative");
-    return RESULT_ERR_INVALID_ARG;
-  }
-  
+ if (turning_angle > 360){
+   LOGE(TAG, "Angle value redundant: %f", turning_angle);
+   return RESULT_ERR_INVALID_ARG;
+ }
+ if (turning_radius < 0){
+   LOGE(TAG, "turning radius can't be negative: %f", turning_radius);
+   return RESULT_ERR_INVALID_ARG;
+ }
+ if (distance_to_go < 0){
+   LOGE(TAG, "distance to go can't be negative: %f", distance_to_go);
+   return RESULT_ERR_INVALID_ARG;
+ }
   DrivingBoardMotorMsg message = DrivingBoardMotorMsg_init_zero;
-    message.distance_to_go = distance_to_go;
-    message.turning_angle = turning_angle;
-    message.turning_radius = turning_radius;
-    *encoding_out = pb_message_encode((void *)&message, DrivingBoardMotorMsg_fields);
-    if (encoding_out->result != RESULT_OK) {
-      LOGE(TAG, "Encoding error: %s", result_to_short_str(encoding_out->result));
-      return RESULT_FAIL;
-  }
-    else {
-      LOGE(TAG, "message encoded successfully: %s", result_to_short_str(encoding_out->result));
-  }
+   message.distance_to_go = distance_to_go;
+   message.turning_angle = turning_angle;
+   message.turning_radius = turning_radius;
+   *encoding_out = pb_message_encode((void *)&message, DrivingBoardMotorMsg_fields);
+   if (encoding_out->result != RESULT_OK) {
+     LOGE(TAG, "Encoding error: %s", result_to_short_str(encoding_out->result));
+     return RESULT_FAIL;
+ }
+   else {
+     LOGE(TAG, "message encoded successfully: %s", result_to_short_str(encoding_out->result));
+ }
 }
 //rn DrivingBoardMotorMsg can come from test
