f0ed9254e19d8a16ec41845813f3239f3a6bee17
Jaron Lendering <jaronlendering@gmail.com>
2026-01-08 16:11:25 +0100

    created a long message and a bigger message

--- src/arm_board/main.c
+++ src/arm_board/main.c
@@ -95,7 +95,7 @@ void MainTask(void *argument) {
   while (1) {
     ETH_udp_send(ip, 7, "udp message");
     osDelay(100);
-    ETH_raw_send(mac, "raw message");
+    ETH_raw_send(mac, "long ass raw message looooong looooooonger looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongest");
     osDelay(100);
   }
 }
