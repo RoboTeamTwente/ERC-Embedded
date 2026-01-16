cbffa90931da6b5544aee3fc93ad3064baa4c90a
İrem Çıvgıner <i.civginer@student.utwente.nl>
2026-01-16 01:43:10 +0100

    deleted simulink files for now

--- components/driving_board/simulink/motorLF1_446c021e_49_ds_externals.h
+++
@@ -1,4 +0,0 @@
-/* Simscape target specific file.
- * This file is generated for the Simscape network associated with the solver block 'motorLF1/motorLF1/Solver Configuration'.
- */
-/* external function headers */
