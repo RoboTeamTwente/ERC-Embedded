08372a11213ef5e3a73f3c385bc5816cb0232bc4
Nick Diamantopoulos <diamantonikolas@gmail.com>
2026-01-20 15:18:29 +0100

    mostly change networking constants

--- components/common/networking_constants/networking_constants.h
+++ components/common/networking_constants/networking_constants.h
@@ -3,14 +3,14 @@
 
 // BOARD IDS
 
-// Non-mechanical components: 0x0-0x30 (excusive)
-#define MAIN_BOARD_ID 0x01
+#define DEBUGGING_BOARD_ID 0x01
 #define WIRELESS_BOARD_ID 0x02
 #define SENSOR_BOARD_ID 0x03
 #define PROCESSING_BOARD_ID 0x04
-// Mechanical components: 0x30-0x60 (exclusive)
-#define DRIVING_BOARD_ID 0x21
-#define ROBOTIC_ARM_ID 0x22
-#define SURFACE_SAMPLE_CONTAINER_ID 0x23
+#define DRIVING_BOARD_ID 0x05
+#define ROBOTIC_ARM_ID 0x06
+#define SURFACE_SAMPLE_CONTAINER_ID 0x07
+
+// PACKET IDS
 
 #endif // !NETWORKING_CONSTANTS_H
