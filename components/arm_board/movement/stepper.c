69167e545263ce617b4ff25dbfbe860f4e5e1b1b
Lisa tb <lisatebraak@gmail.com>
2026-01-28 17:13:40 +0100

    stepper i barely know her

--- components/arm_board/movement/stepper.c
+++ components/arm_board/movement/stepper.c
@@ -2,10 +2,36 @@
 #include "result.h"
 #include "stepper.h"
 
+
+//Alledgedly, the steppers will be using CL57T drivers!
+/*  CL57T driver
+    Compatible with NEMA23 motor
+    DEFAULT microstep resolution: 1600
+    DEFAULT direction: counter clockwise
+    ((I *think* that that means that 1 is CW and 0 is CCW))
+    DEFAULT pulse mode: single pulse
+
+    Pulse width should always be greater than 2.5 microsecs!!
+*/
+
+//Placeholder pins
+#define PLS_PIN 2 //Pulse signal
+#define DIR_PIN 3 //Direction signal
+#define ENA_PIN 4 //Enable signal
+#define ALM_PIN 5 //Alarm signal (OUT)
+
 //NOTE: placeholder values
 #define PWM_FREQ 1000
 #define STEPS_PER_REV 200 //In steps / rev
 #define RPM 100 
+
+//The angles from where to where the stepper is allowed to move
+//For example, the wrist could probably not move fully where the arm is
+const uint32_t START_ANGLE_CW = 315; //Place in the CW system where the stepper can only move forwards
+const uint32_t START_ANGLE_CW = 270; //Place in the CW system where the stepper can only move backwards
+const uint32_t START_ANGLE_CCW = START_ANGLE_CW % 360;
+const uint32_t STOP_ANGLE_CCW = START_ANGLE_CW % 360;
+
 
 
 const uint32_t WAVE_DRIVE[4][4] = {
@@ -36,6 +62,12 @@ const uint32_t FULL_DRIVE[4][4] = {
 
 //1:1 recration of protobuf
 typedef struct {
+    uint32_t current_angle;
+    uint32_t pwm;
+} stepper_t;
+
+//1:1 recration of protobuf
+typedef struct {
     float control_wrist_rotation;         
     float control_gripper_pitch;          
     float control_base;
@@ -47,16 +79,24 @@ typedef struct {
     float stepper_bottom_REV;             
 } control_signals_t;
 
-result_t Control_Signals_Handler (control_signals_t signals) {
+stepper_t stepper;
+
+result_t init_stepper() {
+    set_pin(ENA_PIN, "HIGH");
+    stepper.current_angle = 0;
+    stepper.dir = 1; 
+    stepper.pwm = 0;
+}
+
+result_t control_signals_handler (control_signals_t signals) {
 
 }
 
-result_t rotate_stepper(uint32_t angle, bool direction) {
+result_t stepper_make_steps(uint32_t angle, bool direction) {
     angle = angle % 360;
     uint32_t steps_for_angle = STEPS_PER_REV * (angle / 360);
     
-    set_pin("ENA", ">threshold"); //Set enable pin volt. high enough to enable stepper
-    set_pin("DIR", direction); //1 for clockwise, 0 for counterclockwise
+    set_pin(DIR_PIN, direction); //1 for clockwise, 0 for counterclockwise
     
     int seq_len = len(HALF_DRIVE);
     for (int x = 0; x < steps_for_angle; x++) {
@@ -73,9 +113,29 @@ result_t rotate_stepper(uint32_t angle, bool direction) {
     }
 }
 
+result_t rotate_stepper(uint32_t target_angle) { //Target angle is in CW system
+    uint32_t current_angle = stepper.current_angle;
+    uint32_t CW_angle = target_angle - current_angle;
+    uint32_t CCW_angle = 360 - CW_angle;
+
+    if (CW_angle < CCW_angle) {
+        stepper_make_steps(CW_angle, 1);
+    } else if (CCW_angle < CW_angle) {
+        stepper_make_steps(CW_angle, 0);
+    }
+    stepper.current_angle = target_angle;
+}
+
+//placeholder 
+void delayMicroseconds (uint32_t ms){
+//   __HAL_TIM_SET_COUNTER(&htim1, 0);
+//   while (__HAL_TIM_GET_COUNTER(&htim1) < ms);
+    return;
+}
+
 void delay_by_rpm() {
     int ms_in_minute = 60000000;
-    delay(ms_in_minute/STEPS_PER_REV/RPM);
+    delayMicroseconds(ms_in_minute/STEPS_PER_REV/RPM);
 }
 
 //NOTE: for sequence half drive
