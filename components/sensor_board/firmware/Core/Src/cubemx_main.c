fbc363aaa1af00a4c1abc0e667a1bc6a0e0c4102
Shishir Sudeesh Nambiar <shishirnam11@gmail.com>
2026-05-18 16:21:40 +0200

    cleanup

--- components/sensor_board/firmware/Core/Src/cubemx_main.c
+++ components/sensor_board/firmware/Core/Src/cubemx_main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "logging.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
