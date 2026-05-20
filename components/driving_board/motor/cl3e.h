a526cf61af49b5cd84006663dccc62b7ccd4d26b
İrem Çıvgıner <i.civginer@student.utwente.nl>
2026-05-20 13:53:47 +0200

    cl3e final

--- components/driving_board/motor/cl3e.h
+++ components/driving_board/motor/cl3e.h
@@ -4,37 +4,11 @@
 #include <stdint.h>
 #include "stm32h7xx_hal.h"
 
-#define LF_DIR_PORT GPIOB
-#define LF_DIR_PIN  GPIO_PIN_0
 
-#define LM_DIR_PORT GPIOB
-#define LM_DIR_PIN  GPIO_PIN_1
-
-#define LB_DIR_PORT GPIOB
-#define LB_DIR_PIN  GPIO_PIN_2
-
-// RIGHT SIDE
-#define RF_DIR_PORT GPIOB
-#define RF_DIR_PIN  GPIO_PIN_10
-
-#define RM_DIR_PORT GPIOB
-#define RM_DIR_PIN  GPIO_PIN_11
-
-#define RB_DIR_PORT GPIOB
-#define RB_DIR_PIN  GPIO_PIN_12
 
 void CL3E_SetFrequency(TIM_HandleTypeDef *htim, uint32_t channel, uint32_t freq);
 uint32_t CL3E_ControlToFreq(real_T u);
 void CL3E_DriveFromControl(TIM_HandleTypeDef *htim, uint32_t channel, GPIO_TypeDef *dir_port, uint16_t dir_pin, real_T u);
 uint32_t CL3E_GetTimerClock(TIM_HandleTypeDef *htim);
-/**
- * void CL3E_Forward(uint32_t freq);
-void CL3E_Backward(uint32_t freq);
-void CL3E_Stop(void);
-
- */
-
-
- //void CL3E_Test(void);
 
 #endif
