4630b4d79bf524c0762a2094d54f0e2d116168b9
İrem Çıvgıner <i.civginer@student.utwente.nl>
2026-05-28 15:25:49 +0200

    logging remverd

--- components/common/motor_driver/cubemars_ak/cubemars_ak.c
+++ components/common/motor_driver/cubemars_ak/cubemars_ak.c
@@ -82,10 +82,7 @@ result_t cubemars_ak_parse_can_feedback(const FDCAN_RxHeaderTypeDef* rx_header,
     out->motor_temperature =
         (int8_t)data[6] / CUBEMARS_AK_CAN_TEMPERATURE_SCALE;
     out->status_code = (cubemars_ak_error_code)data[7];
-    LOGI(TAG, "Feedback; ID: %d, A: %d, Pos: %d, Speed: %d, Temp: %d",
-         out->motor_id, out->motor_current, out->motor_position,
-         out->motor_speed, out->motor_temperature);
-
+    
     return RESULT_OK;
 }
 
